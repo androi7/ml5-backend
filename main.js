04c5cc1a5780526e33c7cff979eaa1fe485027df
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-02-24 12:19:13 +1100

    websocket for chat

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const express = require('express');
+
 // const bodyParser = require('body-parser');
 const user = require("./routes/user");
+// const chat = require("./routes/chat");
 const InitiateMongoServer = require('./config/db');
 const cors = require('cors');
 
 InitiateMongoServer();
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
@@ -25,7 +28,49 @@ app.get("/", (req, res) => {
 */
 
 app.use("/user", user);
+// app.use("/chat", chat);
 
-app.listen(PORT, (req, res) => {
+// Fallback route handler
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const server = app.listen(PORT, (req, res) => {
   console.log(`Server is listening on PORT ${PORT}`);
+});
+
+let message = null;
+
+const io = require('socket.io')(server);
+
+//.of('/chatroom')
+io.on('connection', socket => {
+
+
+
+    socket.on('message', data => {
+      message = data.message;
+      io.emit('all messages', {
+        message: message
+      });
+    });
+
+
+
+    socket.emit('request', {
+      welcome: `Welcome`
+    });  // emit an event to the socket
+
+    // io.emit('welcome', {
+    //   newUser: 'joined!'
+    // }
+    // );
+
+
+
+
+    // io.emit('joined', { // emit an event to all connected sockets, broadcast
+    //   content: `User ${user.username} joined the chat.`,
+    // });
+
 });
---
+++ routes/chat.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const auth = require("../middleware/auth");
+
+const User = require("../model/User");
+const router = express.Router();
+
+
+module.exports = router;
